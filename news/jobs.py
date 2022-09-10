8c28057753d8291967b295b0248030ae7294f2e2
Chengetanaim <87484844+Chengetanaim@users.noreply.github.com>
2022-09-10 05:04:07 -0700

    Update jobs.py

--- news/jobs.py
+++ news/jobs.py
@@ -1,12 +1,12 @@
 from .iharare_jobs import iharare_jobs_links, iharare_jobs_titles
 # from .jobszimbabwe import jobszimbabwe_dates, jobszimbabwe_links, jobszimbabwe_titles
-from .unjobs import unjobs_links, unjobs_titles
+from .unjobs import un_links, un_titles
 from .models import Job
 
 
 
-titles = iharare_jobs_titles + unjobs_titles
-links = iharare_jobs_links + unjobs_links 
+titles = iharare_jobs_titles + un_titles
+links = iharare_jobs_links + un_links 
 
 
 def add_job():
